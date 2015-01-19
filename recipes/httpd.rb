f164d5c850d415462eea7915b519bea94bc57a80
kseta <keigo.seta@gmail.com>
2015-01-19 22:50:43 +0900

    [recipes] バーチャルホストを設定する機能を追加

--- recipes/httpd.rb
+++ recipes/httpd.rb
@@ -10,3 +10,23 @@
 include_recipe 'apache2'
 include_recipe 'apache2::mod_rewrite'
 include_recipe 'apache2::mod_ssl'
+
+node['cookbook-bms']['app_configurations'].each do | conf |
+  file "/etc/httpd/sites-available/#{conf[:name]}.conf" do
+    action :delete
+  end
+
+  file "/etc/httpd/sites-enabled/#{conf[:name]}.conf" do
+    action :delete
+  end
+
+  web_app conf[:name] do
+    server_name conf[:server_name]
+    server_aliases [ conf[:name] ]
+    allow_override 'All'
+    error_log_dir conf[:error_log_dir]
+    access_log_dir conf[:access_log_dir]
+    docroot conf[:docroot]
+    template 'web_app.conf.erb'
+  end
+end
