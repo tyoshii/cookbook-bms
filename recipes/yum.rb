6c291918d465c88e539d430c0abf2a9a22c6bf45
kseta <keigo.seta@gmail.com>
2015-01-19 12:12:39 +0900

    [recipes] yum パッケージをインストールする recipe を追加

---
+++ recipes/yum.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: cookbook-bms
+# Recipe:: yum
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node['yum']['packages'].each do |name|
+  yum_package "install #{name}" do
+    package_name name
+    action :install
+  end
+end
